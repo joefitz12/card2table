b6fe6428b345a329bc4c6414feeaeda1c9ee880a
Joe Fitzpatrick <joe@joefitzpatrick.dev>
2024-02-25 12:27:34 -0800

    Adds SvelteKit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../styles/app.css';
+</script>
+
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/list">To-Do list</a>
+		</li>
+		<li>
+			<a href="/admin/paid-status">Account status</a>
+		</li>
+		<li>
+			<a href="/admin/user-settings">User settings</a>
+		</li>
+	</ul>
+</nav>
+
+<ol />
+
+<slot />
