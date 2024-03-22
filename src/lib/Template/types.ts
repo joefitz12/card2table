3e28558f86a96e0c659a9c8c474a42d9b30b1b98
Joe Fitzpatrick <joe@Joes-MBP.lan>
2024-03-22 14:19:40 -0700

    Menu (WIP)
    Template
    - Updates types
    - Updates store

--- src/lib/Template/types.ts
+++ src/lib/Template/types.ts
@@ -2,26 +2,46 @@ export type Unit = 'in' | 'cm';
 export type FontSizes = {
 	[x: string]: number;
 };
-export type CardState = {
+
+type Padding = {
+	topPadding: number,
+	rightPadding: number,
+	bottomPadding: number,
+	leftPadding: number
+}
+
+type Border = {
+	borderColor: string;
+	borderRadius: number;
+	borderWidth: number;
+}
+
+type Font = {
+	fontSize: number;
+	fontStyle: string;
+	fontWeight: '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
+}
+
+export type CardState = Partial<Padding> & {
 	title?: string;
 	unit?: Unit;
-	borderRadius?: string;
 	width?: number;
 	height?: number;
 	backgroundColor?: string;
 	color?: string;
 	borderColor?: string;
+	borderRadius?: number;
+	borderWidth?: number;
 	textElements: TextElement[];
 };
 
-export type TextElement = {
+
+
+export type TextElement = Font & Padding & {
 	id: string;
 	title: string;
-	fontSize: number;
-	fontWeight: string;
 	color: string;
 	textDecoration: string;
-	fontStyle: string;
 	leftTransform?: number;
 	topTransform?: number;
 };
